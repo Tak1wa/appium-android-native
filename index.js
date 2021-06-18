0501146ac74e06b0d251e98c4deff23e4f3ef22b
iwasatakahito <iwasa.takahito@classmethod.jp>
2021-06-18 21:17:32 +0900

    fix screenshot.

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 const wdio = require("webdriverio");
 const assert = require("assert");
+const fsp = require("fs").promises;
 
-// const opts = {
-//   path: '/wd/hub',
-//   port: 4723,
-//   capabilities: {
-//     platformName: "Android",
-//     platformVersion: "11",
-//     deviceName: "Android Emulator",
-//     app: "/Users/iwasa.takahito/Downloads/app-debug.apk",
-//     appPackage: "com.tak1wa.hellohello",
-//     appActivity: ".MainActivity",
-//     automationName: "UiAutomator2"
-//   }
-// };
 const opts = {
   path: '/wd/hub',
   port: 4723,
-  capabilities: {}
+  capabilities: {
+    //     platformName: "Android",
+    //     platformVersion: "11",
+    //     deviceName: "Android Emulator",
+    //     app: "/Users/iwasa.takahito/Downloads/app-debug.apk",
+    //     appPackage: "com.tak1wa.hellohello",
+    //     appActivity: ".MainActivity",
+    //     automationName: "UiAutomator2"
+  }
 };
 
 async function main () {
   const client = await wdio.remote(opts);
   const txt1 = await client.$(await client.findElement("id", "com.tak1wa.hellohello:id/txtParam1"));
   await txt1.setValue("2");
+  await client.saveScreenshot(process.env.SCREENSHOT_PATH + "/1.png");
 
   const txt2 = await client.$(await client.findElement("id", "com.tak1wa.hellohello:id/txtParam2"));
   await txt2.setValue("3");
-
+  await client.saveScreenshot(process.env.SCREENSHOT_PATH + "/2.png");
+  
   const btn = await client.$(await client.findElement("id", "com.tak1wa.hellohello:id/btnInput"));
   await btn.click();
-
+  await client.saveScreenshot(process.env.SCREENSHOT_PATH + "/3.png");
+  
   const lbl = await client.$(await client.findElement("id", "com.tak1wa.hellohello:id/lblOutput"));
   const value = await lbl.getText();
   assert.strictEqual(value,"5");
-
-  let screenshot = await client.takeScreenshot();
 
   await client.deleteSession();
 }
